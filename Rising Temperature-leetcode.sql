09c8f7b35fd784c4c2bc1e7cc569142660d5290a
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-02-21 20:00:44 +0600

    Update Rising Temperature-leetcode.sql

--- Rising Temperature-leetcode.sql
+++ Rising Temperature-leetcode.sql
@@ -1,1 +1,7 @@
-
+select 
+    Weather.Id as `Id`
+from
+    `Weather`
+        join
+    `Weather` w on datediff(Weather.RecordDate, w.RecordDate) = 1
+        and Weather.Temperature > w.Temperature
